cdeaa52e466bd61ce0e14a87313c564ba894d909
Steve Jonk <stevejonk1993@gmail.com>
2022-12-28 14:43:56 +0100

    feat: show all pages

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
-<?php get_header() ?>
+<?php get_header(); ?>
+
+
 
 <div class="navigation">
-
+    <div class="navigation__header">
+        <img class="navigation__header__logo"
+            src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-wit.png" />
+    </div>
+    <div class="navigation__body"></div>
 </div>
 
 <div class="content">
+
     <?php
+    query_posts(array(
+        'post_type' => 'page',
+        'orderby' => 'menu_order',
+        'order' => 'ASC'
+    ));
 
     while (have_posts()) {
         the_post();
-        the_content();
-    } ?>
+        get_template_part('template-parts/page-section');
+    }
+
+    wp_reset_query(); ?>
+
 </div>
+
 <?php get_footer() ?>
---
+++ template-parts/page-section.php
@@ -0,0 +1,6 @@
+<section id="<?php the_title(); ?>">
+    <h2><?php the_title(); ?></h2>
+    <div class="page">
+        <?php the_content(); ?>
+    </div>
+</section>
