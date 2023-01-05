a6d601d4367b5ea9740415fbfc84702e427ea197
Steve Jonk <stevejonk1993@gmail.com>
2023-01-05 18:33:16 +0100

    feat: add good img transition

--- ts/animations/animateImageOnScroll.ts
+++ ts/animations/animateImageOnScroll.ts
@@ -7,16 +7,22 @@ const imgSelector = '.page img'
 export const animateImageOnScroll = () => {
   const scrollElements = gsap.utils.toArray(imgSelector)
   scrollElements.forEach((element: any) => {
-    gsap.set(element, {
+    gsap.from(element, {
       opacity: 0,
       y: 24,
+      x: 30,
+      duration: 0.8,
     })
 
-    element.anim = gsap.to(element, {
+    gsap.to(element, {
       opacity: 1,
       y: 0,
+      x: 0,
       scrollTrigger: {
+        end: '0 60%',
+        start: '0 100%',
         trigger: element,
+        scrub: 0.5,
       },
     })
   })
